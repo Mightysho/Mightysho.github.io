587ae52ddc2d9c56961102338d67b729b8e8fc67
Mightysho <82241991+Mightysho@users.noreply.github.com>
2021-05-02 00:04:38 +0100

    Update behavior.js

--- behavior.js
+++ behavior.js
@@ -2,14 +2,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
     alert('Hello! Mightysho welcome you..!');
     alert('Kindly click on the small image on my profile.. See the magic');
 });
-
+thumbnailElement.addEventListener("click", function() {
 var thumbnailElement = document.getElementById("smart_thumbnail");
 thumbnailElement.addEventListener("click", function() {
      alert('I saw you click!');
 });
 if (thumbnailElement.className == "") {
         thumbnailElement.className = "small";
-
 }else {
         thumbnailElement.className = "";
     }
