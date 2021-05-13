bf1eabe0850e781626cf660b5492e777a3b40d89
Mightysho <82241991+Mightysho@users.noreply.github.com>
2021-05-13 22:41:53 +0100

    Add files via upload

---
+++ behavior.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    alert('Hello! Mightysho welcome you..!');
+    alert('Kindly click on the small image on my profile.. See the magic.. Click again.. Ignore PopUp after done.. It will keep coming.');
+});
+document.addEventListener("DOMContentLoaded", function(event) {
+
+var thumbnailElement = document.getElementById("smart_thumbnail");
+thumbnailElement.addEventListener("click", function() {
+if (thumbnailElement.className == "") {
+        thumbnailElement.className = "small";
+}else {
+        thumbnailElement.className = "";
+    }
+  });
+});
