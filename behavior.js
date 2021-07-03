a056455185a9f8dd8ea609f2504624dfe76d919a
Mightysho <82241991+Mightysho@users.noreply.github.com>
2021-07-03 09:19:58 +0100

    Update behavior.js

--- behavior.js
+++ behavior.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     alert('Hello! Mightysho welcome you..!');
-    alert('Kindly click on the small image on my profile.. See the magic.. Click again.. Ignore PopUp after done.. It will keep coming.');
+    alert('Mightysho say Update is currently going on the website, kindly check back later. Thanks!');
+//     alert('Kindly click on the small image on my profile.. See the magic.. Click again.. Ignore PopUp after done.. It will keep coming.');
 });
 document.addEventListener("DOMContentLoaded", function(event) {
 
